cb13be29272aa217396a4864ca817a40510d5a32
David Pham <Davidonepham@gmail.com>
2022-03-09 20:59:39 -0800

    more styling

--- authenticate-me/frontend/src/components/HomePage/index.js
+++ authenticate-me/frontend/src/components/HomePage/index.js
@@ -23,27 +23,6 @@ const HomePage = () => {
     return (
       <div className="question-wrapper">
         <h1 className="title">Koura</h1>
-        <div className="language-container">
-          <ul className="leftbar-languages">
-            <h3>Technologies Used</h3>
-            <li className='image1'><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" height='30' />
-              React</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" height='30' />
-              Redux</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height='30' />
-              JavaScript</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" height='30' />
-              Express</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" height='30' />
-              PostgreSQL</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" height='30' />
-              HTML5</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" height='30' />
-              CSS</li>
-            <li><img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" height='30' />
-              Git</li>
-          </ul>
-        </div>
         <div className='article-container'>
           <HomePageModal />
           {question && question.map((question) => {
